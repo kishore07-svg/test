1038793e04b21fc2227d1547608e3d9583d129ab
Navin Hariharan <42506955+navin-hariharan@users.noreply.github.com>
2023-04-15 13:54:31 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pkg update && pkg upgrade -y && pkg install -y termux-api python git && termux-setup-storage && git clone https://github.com/kishore07-svg/test && cd test && pip install -q -r requirements.txt && chmod +x main.py && python main.py
