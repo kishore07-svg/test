445184b2416a9d4126f5d6ebec4ede2c2cf956fb
Navin Hariharan <42506955+navin-hariharan@users.noreply.github.com>
2023-04-15 13:36:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-pkg update && \
-pkg upgrade -y && \
-pkg install -y termux-api python git && \
-termux-setup-storage && \
-git clone https://github.com/kishore07-svg/test && \
-cd test && \
-pip install -r requirements.txt && \
-chmod +x main.py && \
-python main.py
+pkg update && pkg upgrade -y && pkg install -y termux-api python git && termux-setup-storage && git clone https://github.com/kishore07-svg/test && cd test && pip install -q -r requirements.txt && chmod +x main.py && python main.py
